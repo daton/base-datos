e6fabbe028edcd1074e083db0bcc005ace6dc1d8
campitos <rapidclimate@gmail.com>
2018-06-16 10:45:00 -0500

    sasa

--- base-datos/src/java/com/jc/controladores/ServletAutenticarOracle.java
+++ base-datos/src/java/com/jc/controladores/ServletAutenticarOracle.java
@@ -5,8 +5,10 @@
  */
 package com.jc.controladores;
 
+import com.jc.model.Conexion;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,72 +20,23 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class ServletAutenticarOracle extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        try {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ServletAutenticarOracle</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ServletAutenticarOracle at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        } finally {
-            out.close();
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
+ 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        response.setContentType("text/html;charset=UTF-8");
+        //response.setContentType("application/vnd.ms-excel");
+        
+        PrintWriter out = response.getWriter();
+     try{
+     Conexion.conectarse(request.getParameter("usuario"),
+             request.getParameter("password"),
+             request.getParameter("sid"));
+    RequestDispatcher despachador=  request.getRequestDispatcher("/sitio.html");
+      despachador.forward(request, response);
+      
+     }catch(Exception e){ 
+     RequestDispatcher despachador=  request.getRequestDispatcher("/error.html");
+      despachador.forward(request, response);  
+     }
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
