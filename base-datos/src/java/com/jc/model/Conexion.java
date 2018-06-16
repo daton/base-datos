d38f39a1eb7d2e1374688c0a0f434dd06e2fedd2
campitos <rapidclimate@gmail.com>
2018-06-16 09:07:46 -0500

    sasa

--- base-datos/src/java/com/jc/model/Conexion.java
+++ base-datos/src/java/com/jc/model/Conexion.java
@@ -21,6 +21,13 @@ public class Conexion {
        return con;
     }
     
+    public static Connection conectarse(String usuario, String password,String sid)throws Exception{
+        String url="jdbc:oracle:thin:@localhost:1521:"+sid;
+       Class.forName("oracle.jdbc.OracleDriver");
+       Connection con=DriverManager.getConnection(url,usuario,password);
+       return con;
+    }
+    
     public static Connection conectarse()throws Exception{
         String url="jdbc:oracle:thin:@localhost:1521:xe";
        Class.forName("oracle.jdbc.OracleDriver");
--- base-datos/src/java/com/jc/model/TestRefCursor.java
+++ base-datos/src/java/com/jc/model/TestRefCursor.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.jc.model;
 
 import java.sql.CallableStatement;
@@ -16,17 +15,18 @@ import oracle.jdbc.internal.OracleTypes;
  * @author campitos
  */
 public class TestRefCursor {
-    public static void main(String args[])throws Exception{
-        Conexion c=new Conexion();
-        Connection con=c.conectarse();
-     CallableStatement callate=con.prepareCall("{call buscar_peliculas(?)}");
-        callate.registerOutParameter(1,OracleTypes.CURSOR);
-       
+
+    public static void main(String args[]) throws Exception {
+        Conexion c = new Conexion();
+        Connection con = c.conectarse();
+        CallableStatement callate = con.prepareCall("{call buscar_peliculas(?)}");
+        callate.registerOutParameter(1, OracleTypes.CURSOR);
+        
         callate.execute();
-     ResultSet res=(ResultSet) callate.getObject(1);
-       while(res.next()){
-           System.out.println("Titulo "+res.getString(2));
-       }
+        ResultSet res = (ResultSet) callate.getObject(1);
+        while (res.next()) {
+            System.out.println("Titulo " + res.getString(2)+" el id "+res.getInt(1));
+        }
     }
     
 }
